9000842eb437820399dc4228c08cab6332c12ba8
thiagoap <thiago_ap_@hotmail.com>
2024-09-24 21:21:11 -0300

    tela de login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,25 +4,33 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">  
         
-       <link rel="stylesheet" href="/css/css_bootstrap_minify.css">
+        <link rel="stylesheet" href="/assets/css/login.css">
         <title>Document</title>
-        
+        @include('bootstrap/css')
     </head>
     <body>
 
-        <div class="form-floating mb-3">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
+        <div class="login">
+            <form >
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control border border-primary" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control border border-primary" id="exampleInputPassword1">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input border border-primary" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
-            
-        <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-        </div>
-        
         
     </body>
 
-    <script src="/js/js_bootstrap-5.3.3-dist.js"></script>
+    @include('bootstrap/js')
 
 </html>
