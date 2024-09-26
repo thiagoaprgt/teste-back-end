da1206b91eefcf32ecce6c5fa51d86bc4e12732f
thiagoap <thiago_ap_@hotmail.com>
2024-09-26 13:43:32 -0300

    Está funcionando a exlusão de categorias

---
+++ public/assets/js/products/getAllProducts.js
@@ -0,0 +1,79 @@
+const submitAddCategory = document.querySelector("#submitAddCategory");
+const submitDeleteCategory = document.querySelector("#submitDeleteCategory");
+
+async function categoryCreated() {  
+    
+    document.querySelectorAll('#message')[0].innerHTML = 'Categoria cadastrada com sucesso';
+    document.querySelectorAll('#message')[0].classList.toggle("visually-hidden");
+
+    setTimeout(() => {
+
+        document.querySelectorAll('#message')[0].innerHTML = '';
+        document.querySelectorAll('#message')[0].classList.toggle("visually-hidden");
+
+        window.location.reload();
+        
+    }, 4000);
+
+    
+}
+
+
+async function categoryDeleted() {  
+    
+    document.querySelectorAll('#message')[0].innerHTML = 'Categoria Deletada com sucesso';
+    document.querySelectorAll('#message')[0].classList.toggle("visually-hidden");
+
+    setTimeout(() => {
+
+        document.querySelectorAll('#message')[0].innerHTML = '';
+        document.querySelectorAll('#message')[0].classList.toggle("visually-hidden");
+
+        window.location.reload();
+        
+    }, 4000);
+
+    
+}
+
+    
+
+submitAddCategory.addEventListener("click", async () => {            
+    
+    const formData = new FormData();
+    formData.append( "_token", document.querySelectorAll('#laravelSecurityToken')[0].value );    
+    formData.append( "category", document.querySelectorAll('#selectcategories')[0].value );
+
+    console.log(formData);
+
+    const response = await fetch("/createProduct", {
+        method: "POST",
+        body: formData,
+    });    
+     
+    await categoryCreated();
+  
+
+});
+
+
+
+submitDeleteCategory.addEventListener("click", async () => {            
+    
+    const formData = new FormData();
+    formData.append( "_token", document.querySelectorAll('#laravelSecurityToken')[0].value );
+    formData.append( "category", document.querySelectorAll('#selectcategories')[0].value );
+    
+
+    console.log(formData);
+
+    const response = await fetch("/deleteCategory", {
+        method: "POST",
+        body: formData,
+    });    
+     
+    await categoryDeleted();
+  
+
+});
+
